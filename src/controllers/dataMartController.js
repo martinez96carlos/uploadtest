712d58e20f93dd9c09fef42665a51d0e338309bf
Carlos Martinez <martinez96carlos@gmail.com>
2020-06-02 16:09:07 -0400

    end points del datamart primeros 8 graficos

--- src/controllers/dataMartController.js
+++ src/controllers/dataMartController.js
@@ -1,13 +1,116 @@
 const pool = require('../database/connectiondm');
 
-const obtenerVolumenPorTipoDeResiduo = async (req,res) => {
+// GRAFICOS GENERALES
+const getVolumenPorResiduoGeneral = async (req,res) => {
     const response = await pool.query(`
-    /*Pesos por residuo GENERAL*/
-    select C.solid_type_id, C.solid_type_name, Sum(A.weight)
-    from fact_recolection A, dim_solids B, dim_solid_types C
-    where A.solid_id = B.solid_id and B.solid_type_id = C.solid_type_id
-    group by C.solid_type_name, C.solid_type_id
-    order by C.solid_type_id`,(error, response, fields) => {
+    SELECT A.solid_type_name as tipo_residuo, COALESCE(sum(C.weight),0) as volumen
+    FROM dim_solid_types A
+    LEFT JOIN dim_solids B ON B.solid_type_id = A.solid_type_id
+    LEFT JOIN fact_recolection C ON C.solid_id = B.solid_id
+    group by A.solid_type_id`,(error, response, fields) => {
+        if (!error){
+           res.status(200).json(response.rows);
+        } else {
+           console.log(error);
+        }
+    });
+}
+const getVolumenPorTipoVivienda = async (req,res) => {
+    const response = await pool.query(`
+    select A.generator_place, COALESCE(sum(B.weight),0) as volumen
+    from dim_orders A
+    LEFT JOIN fact_recolection B ON B.order_id = A.order_id
+    group by A.generator_place;`,(error, response, fields) => {
+        if (!error){
+           res.status(200).json(response.rows);
+        } else {
+           console.log(error);
+        }
+    });
+}
+const getVolumenPorMes  = async (req,res) => {
+    const response = await pool.query(`
+    select  TO_CHAR(TO_DATE(A.mes::text, 'MM'),'Month') AS "Mes", COALESCE(sum(B.weight),0) as volumen
+    from dim_time A
+    LEFT JOIN fact_recolection B ON B.time_id = A.time_id
+    group by A.mes;`,(error, response, fields) => {
+        if (!error){
+           res.status(200).json(response.rows);
+        } else {
+           console.log(error);
+        }
+    });
+}
+const getVolumenPorCiudad = async (req,res) => {
+    const response = await pool.query(`
+    select A.city, COALESCE(sum(C.weight),0) as volumen
+    from dim_recolector A
+    LEFT JOIN dim_orders B on B.recolector_id = A.recolector_id 
+    LEFT JOIN fact_recolection C on C.order_id = B.order_id
+    group by A.city`,(error, response, fields) => {
+        if (!error){
+           res.status(200).json(response.rows);
+        } else {
+           console.log(error);
+        }
+    });
+}
+const getTopFiveRecolectors = async (req,res) => {
+    const response = await pool.query(`
+    select A.nombre, A.apellido, A.city, COALESCE(sum(C.weight),0) as volumen
+    from dim_recolector A
+    LEFT JOIN dim_orders B ON B.recolector_id = A.recolector_id
+    LEFT JOIN fact_recolection C ON C.order_id = B.order_id
+    group by A.nombre, A.apellido, A.city
+    limit 5`,(error, response, fields) => {
+        if (!error){
+           res.status(200).json(response.rows);
+        } else {
+           console.log(error);
+        }
+    });
+}
+const getVolumenPorResiduoPersonal = async (req,res) => {
+    const id = req.params.id;
+    const response = await pool.query(`
+    SELECT A.solid_type_name as tipo_residuo, COALESCE(sum(C.weight),0) as volumen
+    FROM dim_solid_types A
+    LEFT JOIN dim_solids B ON B.solid_type_id = A.solid_type_id
+    LEFT JOIN fact_recolection C ON C.solid_id = B.solid_id
+    LEFT JOIN dim_orders D ON D.order_id = C.order_id
+    where D.recolector_id = $1
+    group by A.solid_type_id`,[id],(error, response, fields) => {
+        if (!error){
+           res.status(200).json(response.rows);
+        } else {
+           console.log(error);
+        }
+    });
+}
+const getVolumenPorViviendaPersonal = async (req,res) => {
+    const id = req.params.id;
+    const response = await pool.query(`
+    select A.generator_place, COALESCE(sum(B.weight),0) as volumen
+    from dim_orders A
+    LEFT JOIN fact_recolection B ON B.order_id = A.order_id
+    where recolector_id = $1
+    group by A.generator_place;`,[id],(error, response, fields) => {
+        if (!error){
+           res.status(200).json(response.rows);
+        } else {
+           console.log(error);
+        }
+    });
+}
+const getVolumenPorMesPersonal = async (req,res) => {
+    const id = req.params.id;
+    const response = await pool.query(`
+    select TO_CHAR(TO_DATE(A.mes::text, 'MM'),'Month') AS "Mes", COALESCE(sum(B.weight),0) as volumen
+    from dim_time A
+    LEFT JOIN fact_recolection B ON B.time_id = A.time_id
+    LEFT JOIN dim_orders C on B.order_id = C.order_id
+    where C.recolector_id = $1
+    group by A.mes;`,[id],(error, response, fields) => {
         if (!error){
            res.status(200).json(response.rows);
         } else {
@@ -16,6 +119,20 @@ const obtenerVolumenPorTipoDeResiduo = async (req,res) => {
     });
 }
 
+
+//graficos personales
+
+
+
+
 module.exports = {
-    obtenerVolumenPorTipoDeResiduo
+    getVolumenPorResiduoGeneral,
+    getVolumenPorTipoVivienda,
+    getVolumenPorMes,
+    getVolumenPorCiudad,
+    getTopFiveRecolectors,
+    getVolumenPorResiduoPersonal,
+    getVolumenPorViviendaPersonal,
+    getVolumenPorMesPersonal
+
 }
--- src/routes/datamart.js
+++ src/routes/datamart.js
@@ -1,7 +1,26 @@
 const { Router} = require('express');
 const router = Router();
 
-const  {obtenerVolumenPorTipoDeResiduo } = require('../controllers/dataMartController');
-router.get('/prueba', obtenerVolumenPorTipoDeResiduo);
+const  {getVolumenPorResiduoGeneral,
+    getVolumenPorTipoVivienda,
+    getVolumenPorMes,
+    getVolumenPorCiudad,
+    getTopFiveRecolectors,
+    getVolumenPorResiduoPersonal,
+    getVolumenPorViviendaPersonal,
+    getVolumenPorMesPersonal } = require('../controllers/dataMartController');
+
+//graficos generales
+router.get('/gone',getVolumenPorResiduoGeneral );
+router.get('/gtwo', getVolumenPorTipoVivienda);
+router.get('/gthree',getVolumenPorMes );
+router.get('/gfour', getVolumenPorCiudad);
+router.get('/gfive', getTopFiveRecolectors);
+
+//graficos personales
+router.get('/pone/:id', getVolumenPorResiduoPersonal);
+router.get('/ptwo/:id', getVolumenPorViviendaPersonal);
+router.get('/pthree/:id', getVolumenPorMesPersonal);
+
 
 module.exports = router;
